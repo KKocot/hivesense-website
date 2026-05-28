ce18eb3cd88da51e1f9e04c123309323a8846525
Krzysztof Kocot <k.kocot0@gmail.com>
2026-05-28 15:07:47 +0200

    feat: add links config module

---
+++ src/config/links.ts
@@ -0,0 +1,8 @@
+export const LINKS = {
+  HIVESENSE: "https://gitlab.syncad.com/hive/hivesense",
+  MIT_LICENSE:
+    "https://gitlab.syncad.com/hive/hivesense/-/blob/main/LICENSE",
+  HIVE: "https://hive.io",
+  HIVE_TOOLS: "https://tools.openhive.network",
+  BLOCKCHAIN_WARES: "https://blockchainwares.com.pl/",
+} as const;
